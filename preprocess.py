433c7c43f861b17f41f5b9f21bcafbc948cd4fd6
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-07-25 10:08:49 +0100

    started preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from math import floor, log2
 
 '''
     1) Get raw data files
@@ -11,16 +12,37 @@ import os
 params = {
     'raw_dir': "./data/raw",
     'train_dir': "./data/train",
-    'fields': ["Source IP", "Source Port", "Destination IP", "Destination Port", "Protocol", "Label"]
+    'fields': ["Timestamp", "Source IP", "Destination IP", "Protocol", "Fwd Packet Length Mean", "Label"]
 }
 
 frames = []
+# Iterate through all dataset files and remove duplicates
 for f in os.listdir(params['raw_dir']):
     df = pd.read_csv("{0}/{1}".format(params['raw_dir'], f), skipinitialspace=True, usecols=params['fields'], encoding='latin1')
     df.drop_duplicates(keep=False,inplace=True)
     frames.append(df)
-    print(f)
-    print(df)
+    #print(f)
+    #print(df)
+
+for f in frames:
+    current_time = -1
+    active_dyad = {}
+    for index, row in f.iterrows():
+        c_ip_pair = -1
+        
+        if (row.loc['Source IP'], row.loc['Destination IP']) in active_dyad:
+            c_ip_pair = (row.loc['Source IP'], row.loc['Destination IP'])
+            pass
+        elif (row.loc['Destination IP'], row.loc['Source IP']) in active_dyad:
+            c_ip_pair = (row.loc['Destination IP'], row.loc['Source IP'])
+            pass
+        else:
+            c_ip_pair = (row.loc['Source IP'], row.loc['Destination IP'])
+            active_dyad[c_ip_pair] = []
+
+        active_dyad[c_ip_pair].append((row.loc['Protocol'], floor(log2(row.loc['Fwd Packet Length Mean'])) if row.loc['Fwd Packet Length Mean'] else 0.0))
+
+    print(active_dyad)
 
 combined_df = pd.concat(frames)
 print(combined_df)
